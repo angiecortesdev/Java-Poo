31972b0a36c2f01d14c8f9ea0bf28ded7ba79552
angiecortesdev <angiecortes29@gmail.com>
2023-09-04 10:09:19 -0500

    Emcapsulamiento y herencia

--- prueba/src/App.java
+++ prueba/src/App.java
@@ -1,5 +1,9 @@
 //import main.java.com.softlond.project.Calculadora;
-import main.java.com.softlond.project.poo.herencia.MedioTerrestre;
+import main.java.com.softlond.project.poo.encapsulamiento.Estudiante;
+//import main.java.com.softlond.project.poo.encapsulamiento.Persona;
+//import main.java.com.softlond.project.poo.herencia.MedioTerrestre;
+import main.java.com.softlond.project.poo.encapsulamiento.ModuloInscripcion;
+import main.java.com.softlond.project.poo.encapsulamiento.encapsulamiento2.Foraneo;
 
 
 
@@ -10,14 +14,37 @@ public class App {
 
         
 
-        MedioTerrestre terrestre1 = new MedioTerrestre(1.5f, 45, 4);
+        //MedioTerrestre terrestre1 = new MedioTerrestre(1.5f, 45, 4);
         
-        System.out.println(terrestre1.peso);
-        System.out.println(terrestre1.altura);
-        System.out.println(terrestre1.cantidadLlantas);
+        //System.out.println(terrestre1.peso);
+        //System.out.println(terrestre1.altura);
+        //System.out.println(terrestre1.cantidadLlantas);
 
-        terrestre1.encender();
+        //terrestre1.encender();
 
+        //Persona persona1 = new Persona("Angie", "1033761953", 29);
+
+        //System.out.println(persona1.nombre);
+        //System.out.println(persona1.numeroCedula);
+        //System.out.println(persona1.edad);
+
+
+        Estudiante estudiante1 = new Estudiante("Angie", "10234613", 23);
+        Estudiante estudiante2 = new Estudiante("Kathleen", "389836", 8);
+        Estudiante estudiante3 = new Estudiante("Kamilo", "9735767", 32);
+
+        System.out.println(estudiante1.nombre);
+        System.out.println(estudiante1.numeroCedula);
+        System.out.println(estudiante1.edad);
+
+        ModuloInscripcion sistemaInscripcion = new ModuloInscripcion();
+        sistemaInscripcion.inscribirEstudiante(estudiante1);
+        sistemaInscripcion.inscribirEstudiante(estudiante2);
+        sistemaInscripcion.inscribirEstudiante(estudiante3);
+
+        Foraneo foraneo1 = new Foraneo("Ander", "541638543", 23);
+        
+        foraneo1.testAccesoProtected();
 
 
 
